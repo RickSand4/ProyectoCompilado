c814487ed22483bf5c023d822a84162629891730
Erick <albarazork@gmail.com>
2025-06-05 23:51:08 -0600

    inicio de sesion

---
+++ src/main/java/com/upiiz/compilado/Controllers/LoginController.java
@@ -0,0 +1,12 @@
+package com.upiiz.compilado.Controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class LoginController {
+    @GetMapping("/")
+    public String login() {
+        return "login";
+    }
+}
